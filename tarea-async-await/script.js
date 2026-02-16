32c76a91fdc5c4fd41f6fc19c1454cccc06a1e57
Mijangos3772 <240300710@ucaribe.edu.mx>
2026-02-15 20:29:55 -0500

    Update script.js

--- tarea-async-await/script.js
+++ tarea-async-await/script.js
@@ -1,1 +1,42 @@
+const esperar = (ms) => {
+  return new promise(resolve => {
+    settimeout(() => {
+      resolve("Mensaje timeout tarde llego (2 segundos despues)");
+    }, ms);
+  });
+};
 
+const ejercicio1 = async () => {
+
+  const contenedor = document.getElementById("text-id");
+
+  contenedor.innerHTML = "Iniciando programa...<br>";
+  contenedor.innerHTML += "Fin programa...<br>";
+
+  const mensaje = await esperar(2000);
+
+  contenedor.innerHTML += mensaje;
+};
+
+const ejercicio2 = async () => {
+
+    const contenedor = document.getElementById("text-id2");
+    contenedor.innerHTML = "Consultando API...<br>";
+
+    try {
+        const respuesta = await fetch("https://api.exchangerate-api.com/v4/latest/USD");
+        const datos = await respuesta.json();
+
+        console.log(datos);
+
+        contenedor.innerHTML += `
+            Base: ${datos.base} <br>
+            Tipo de cambio MXN: ${datos.rates.MXN} <br>
+            Tipo de cambio EUR: ${datos.rates.EUR}
+        `;
+
+    } catch (error) {
+        console.error("Error:", error);
+        contenedor.innerHTML += "Error al consultar la API";
+    }
+};
